6e70bcd121a895f97831c416f926dc19ffda3ac6
Konstantin Biriukov <kostya-leon@mail.ru>
2019-03-07 16:48:48 +0700

    fixes: css, DOM, Function Declaration, js restructure

--- TemperatureConversion/src/temperatureConversion.js
+++ TemperatureConversion/src/temperatureConversion.js
@@ -1,36 +1,43 @@
 // • Сделайте страницу для перевода температур
 // • Сделайте поле ввода для температуры в цельсиях
-// • И по нажатию на кнопку, чтобы температура переводилась в шкалы цельсия и фаренгейта,
+// • И по нажатию на кнопку, чтобы температура переводилась в шкалы кельвина и фаренгейта,
 //   и результаты выводились на страницу
 
 "use strict";
-var inputSet = document.getElementsByTagName("input");
-var button = document.getElementsByTagName("button");
 
-var convertFromCelsiusToFahrenheit = function (celsius) {
+function convertFromCelsiusToFahrenheit(celsius) {
     return celsius * 1.8 + 32;
-};
+}
 
-var convertFromCelsiusToKelvin = function (celsius) {
-    return +celsius + 273;
-};
+function convertFromCelsiusToKelvin(celsius) {
+    return +celsius + 273.15;
+}
 
-var roundToOneFractionDigit = function (value) {
-    return value.toFixed(1);
-};
+function roundToTwoFractionDigits(value) {
+    return value.toFixed(2);
+}
 
-button[0].addEventListener("click", function () {
-    var inputValue = Number(inputSet[0].value);
-    if (isNaN(inputValue)) {
-        alert("Type a number...");
-        // to clear all input fields
-        [].forEach.call(inputSet, function (inputElement) {
-            inputElement.value = "";
-        });
-    } else {
-        document.getElementsByTagName("input")[1].value =
-            roundToOneFractionDigit(convertFromCelsiusToFahrenheit(inputValue));
-        document.getElementsByTagName("input")[2].value =
-            roundToOneFractionDigit(convertFromCelsiusToKelvin(inputValue));
-    }
-});
+function readyDOM() {
+    var convertButton = document.getElementById("conversion-button");
+    convertButton.addEventListener("click", function () {
+        var celsiusInput = document.getElementById("celsius");
+        var inputValue = Number(celsiusInput.value);
+
+        if (isNaN(inputValue)) {
+            alert("Type a number...");
+            // to clear all input fields
+            var inputSet = document.getElementsByClassName("conversion-field");
+            [].forEach.call(inputSet, function (inputElement) {
+                inputElement.value = "";
+            });
+
+        } else {
+            document.getElementById("fahrenheit").value =
+                roundToTwoFractionDigits(convertFromCelsiusToFahrenheit(inputValue));
+            document.getElementById("kelvin").value =
+                roundToTwoFractionDigits(convertFromCelsiusToKelvin(inputValue));
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", readyDOM);
