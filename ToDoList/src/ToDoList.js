f6dcfa82b687a47a9f36c077e8fd6bd1c05e1884
Konstantin Biriukov <kostya-leon@mail.ru>
2019-03-11 15:53:56 +0700

    v 1.0 implementation: add and delete functionality

---
+++ ToDoList/src/ToDoList.js
@@ -0,0 +1,46 @@
+//  Создать простую программу TO DO List
+// • Есть список записей (поначалу пустой), поле ввода и кнопка для добавления записи
+// • По нажатию на кнопку в конец списка должна добавиться запись с текстом из поля ввода. При этом поле ввода очищается
+// • Рядом с каждым элементом списка есть кнопка удаления
+// • * Можно еще сделать рядом кнопку редактирования – по нажатию вместо текста появляется поле ввода с текстом,
+// а вместо кнопки редактирования – появляются кнопки «Отменить» и «Сохранить»
+
+var unorderedList = document.getElementById("notes");
+
+function addToList() {
+    var additionButton = document.getElementById("add-button");
+    additionButton.addEventListener("click", function () {
+        var inputField = document.getElementById("add-element-field");
+        var inputDataValue = inputField.value;
+        if (inputDataValue.trim().length === 0) {
+            return;
+        }
+        var listItem = document.createElement("li");
+
+        listItem.innerText = inputDataValue;
+        unorderedList.appendChild(listItem);
+        inputField.value = "";
+        addDeleteItem(listItem);
+    });
+}
+
+function addDeleteItem(listItem) {
+    var imgDelete = document.createElement("input");
+    imgDelete.type = "image";
+    imgDelete.alt = "delete";
+    imgDelete.src = "../resources/deleteImage.png";
+    imgDelete.style.cssText = "height: 18px; \
+                    float: right;\
+                    margin-left: 15px;\
+                    ";
+    listItem.appendChild(imgDelete);
+    deleteItem(imgDelete, listItem);
+}
+
+function deleteItem(imgDelete, listItem) {
+    imgDelete.addEventListener("click", function () {
+        unorderedList.removeChild(listItem);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", addToList);
