9db261d3fd1c8784d457ec1a98782499986e81ea
Konstantin Biriukov <kostya-leon@mail.ru>
2019-03-11 23:56:19 +0700

    v.2.0 implementation: added edit functionality

--- ToDoList/src/ToDoList.js
+++ ToDoList/src/ToDoList.js
@@ -21,27 +21,94 @@ function addToList() {
         unorderedList.appendChild(listItem);
         inputField.value = "";
         addDeleteButton(listItem);
+        addEditButton(listItem);
     });
 }
 
 function addDeleteButton(listItem) {
-    var imgDelete = document.createElement("input");
-    imgDelete.type = "image";
-    imgDelete.alt = "delete";
-    imgDelete.title = "delete";
-    imgDelete.src = "../resources/deleteImage.png";
-    imgDelete.style.cssText = "height: 18px; \
-                    float: right;\
-                    margin-left: 15px;\
-                    ";
-    listItem.appendChild(imgDelete);
-    deleteItem(imgDelete, listItem);
+    var deleteImage = document.createElement("input");
+    loadPicture(deleteImage, "delete", "../resources/deleteImage.png");
+    listItem.appendChild(deleteImage);
+    deleteItem(deleteImage, listItem);
 }
 
-function deleteItem(imgDelete, listItem) {
-    imgDelete.addEventListener("click", function () {
+function deleteItem(deleteImage, listItem) {
+    deleteImage.addEventListener("click", function () {
         unorderedList.removeChild(listItem);
     });
+}
+
+function addEditButton(listItem) {
+    var editImage = document.createElement("input");
+    loadPicture(editImage, "edit", "../resources/editImage.png");
+    editImage.style.marginLeft = "15px";
+    listItem.appendChild(editImage);
+    addEditItem(editImage, listItem);
+}
+
+function addEditItem(imageEdit, listItem) {
+    imageEdit.addEventListener("click", function () {
+        var textList = listItem.textContent;
+        var editableListItem = document.createElement("li");
+        var inputField = document.createElement("input");
+
+        inputField.type = "text";
+        inputField.maxLength = 50;
+        inputField.value = textList;
+        inputField.style.padding = "1px";
+        inputField.style.width = "250px";
+
+        var cancelImage = document.createElement("input");
+        loadPicture(cancelImage, "cancel", "../resources/cancelImage.png");
+
+        var saveImage = document.createElement("input");
+        loadPicture(saveImage, "save", "../resources/saveImage.png");
+        saveImage.style.marginLeft = "15px";
+
+        unorderedList.removeChild(listItem);
+        unorderedList.appendChild(editableListItem);
+        editableListItem.appendChild(inputField);
+
+        editableListItem.appendChild(cancelImage);
+        editableListItem.appendChild(saveImage);
+
+        cancelAction(cancelImage, editableListItem, textList);
+        saveItem(saveImage, editableListItem, inputField);
+    });
+}
+
+function saveItem(saveImage, editableListItem, inputField) {
+    saveImage.addEventListener("click", function () {
+        var textInputField = inputField.value;
+        unorderedList.removeChild(editableListItem);
+        returnToInitialListItemState(textInputField);
+    });
+}
+
+function cancelAction(cancelImage, editableListItem, textList) {
+    cancelImage.addEventListener("click", function () {
+        unorderedList.removeChild(editableListItem);
+        returnToInitialListItemState(textList);
+    });
+}
+
+function returnToInitialListItemState(text) {
+    var listItem = document.createElement("li");
+    listItem.textContent = text;
+    unorderedList.appendChild(listItem);
+    addDeleteButton(listItem);
+    addEditButton(listItem);
+}
+
+function loadPicture(image, name, link) {
+    image.type = "image";
+    image.alt = name;
+    image.title = name;
+    image.src = link;
+    image.style.cssText = "height: 18px; \
+                    float: right;\
+                    margin-left: 3px;\
+                    ";
 }
 
 document.addEventListener("DOMContentLoaded", addToList);
