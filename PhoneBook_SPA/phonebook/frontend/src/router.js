4d21e88f657005bfa4899a0d056a50009fbdcd2b
Konstantin Biriukov <kostya-leon@mail.ru>
2019-05-20 02:33:01 +0700

    fixes

--- PhoneBook_SPA/phonebook/frontend/src/router.js
+++ PhoneBook_SPA/phonebook/frontend/src/router.js
@@ -1,36 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import About from './views/About.vue'
+
 // import PhoneBook from '../../views/index.twig';
 import Page404 from './views/Page404.vue';
 
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    // {
-    //   path: '/phoneBook',
-    //   name: 'phoneBook',
-    //   component: PhoneBook
-    // },
-    {
-      path: "*",
-      component: Page404
-    }
-  ]
-})
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: About
+        },
+        // {
+        //   path: '/phoneBook',
+        //   name: 'phoneBook',
+        //   component: PhoneBook
+        // },
+        {
+            path: "*",
+            component: Page404
+        }
+    ]
+});
--- PhoneBook_SPA/phonebook/frontend/vue.config.js
+++ PhoneBook_SPA/phonebook/frontend/vue.config.js
@@ -1,6 +1,14 @@
 module.exports = {
     outputDir: "../public/dist",
+    filenameHashing: false,
+
     devServer: {
         proxy: 'http://localhost:3000'
+    },
+
+    chainWebpack: config => {
+        config.plugins.delete("html");
+        config.plugins.delete("preload");
+        config.plugins.delete("prefetch");
     }
 };
